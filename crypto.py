e70c268545987c868283d00cafa112d32d785dda
manatk <manatmadan@gmail.com>
2020-09-28 12:17:44 -0700

    updated decrypt + W

--- crypto.py
+++ crypto.py
@@ -1,30 +1,35 @@
-import string, random, math
+import string
+import random
+import math
 # Caesar Cipher
 # Arguments: string, integer
 # Returns: string
 
 def encrypt_caesar(plaintext, offset):
     encrypted_text = ""
+    #loop through each character in the text and shift it
     for character in plaintext:
+        #calling helper function which offsets the character
         encrypted_character = offset_character(character, offset)
         encrypted_text = encrypted_text + encrypted_character
-    #print("Encrypted is", encrypted_text)
     return encrypted_text
 
+#Helper function which offsets characters by a given value. I chose to make a helper function since this program required me to offset a lot of characters.
 def offset_character(character, offset):
-    #print (character)
     if character not in string.ascii_uppercase:
         return character
     else:
+        #get original index of character
         char_index = ord(character) - ord('A')
+        #get offset of character. Modulate to ensure that looping around alphabet
         final_index = (char_index + offset) % 26
-
+        #turn the integer index into the corresponding ASCII index
         return chr(final_index + ord('A'))
 
 # Arguments: string, integer
 # Returns: string
+#Basically the same method as encrypt, but passing a negative offset to the offset helper method
 def decrypt_caesar(ciphertext, offset):
-
     decrypt_offset = offset * -1
     decrypted_text = ""
     for character in ciphertext:
@@ -42,13 +47,16 @@ def encrypt_vigenere(plaintext, keyword):
     ASCII_A = ord('A')
 
     for i in range (0, plaintext_len):
+        #looping the keyword around the plaintext string
         key_char = i % keyword_len
         offset = ord(keyword[key_char]) - ASCII_A
+        #calculating amount of shift
         encrypted_text = encrypted_text + offset_character(plaintext[i], offset)
     return(encrypted_text)
 
 # Arguments: string, string
 # Returns: string
+#same method as encrypt but with a negative offset value
 def decrypt_vigenere(ciphertext, keyword):
     cipher_len = len(ciphertext)
     keyword_len = len(keyword)
@@ -61,7 +69,7 @@ def decrypt_vigenere(ciphertext, keyword):
         decrypted_text = decrypted_text + offset_character(ciphertext[i], offset)
     return(decrypted_text)
 
-
+#generate the coprime value to Q
 def gen_coprime(Q):
     #assigning starter value for R which will change later
     R = Q
@@ -73,10 +81,11 @@ def gen_coprime(Q):
 # Arguments: integer
 # Returns: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
 def generate_private_key(n=8):
+    #defining an arbitrary start value for W
     W_list = [random.randint(1,15)]
-    for i in range(1, n+1):
+    for i in range(1, n):
+        #append another W value to the list
         W_list.append(random.randint(sum(W_list)+1, 2*sum(W_list)))
-
     Q = random.randint(sum(W_list)+1, sum(W_list)*2)
     #only passing Q to gen_coprime because haven't defined R
     R = gen_coprime(Q)
@@ -86,14 +95,14 @@ def generate_private_key(n=8):
 # Arguments: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
 # Returns: tuple B - a length-n tuple of integers
 def create_public_key(private_key):
-
+    #extract elements from private key
     W = private_key[0]
     Q = private_key[1]
     R = private_key[2]
     #Tuple B = tuple where B_i = R*W_i modQ
     B_list = []
-
     for i in range (0, len(W)):
+        #add B values
         B_list.append((R * W[i]) % Q)
     B = tuple(B_list)
     #print (B)
@@ -108,29 +117,30 @@ def encrypt_mhkc(plaintext, public_key):
     C = []
     sum = 0
     for character in plaintext:
+        #for each character in the plaintext, get binary values. ensure getting 8 values.
         for binary in bin(ord(character))[2:].zfill(n):
             bin_value_list.append(int(binary))
         A.append(bin_value_list)
+        #reset bin_value_list for next iteration of loop
         bin_value_list = []
     for binary_list in A:
+        #multiply each binary value by the corresponding public key value
         for i in range(0, len(binary_list)):
             sum = sum + (binary_list[i] * public_key[i])
         C.append(sum)
         sum = 0
     return(C)
 
+#calculate the S value, which is the modular inverse of R % Q
 def calc_S(R,Q):
     potential_S = 1
     while ((potential_S*R) % Q != 1):
         potential_S = random.randint(2,Q)
     return potential_S
 
-
 # Arguments: list of integers, tuple B - a length-n tuple of integers
 # Returns: bytearray or str of plaintext
 def decrypt_mhkc(ciphertext, private_key):
-    print (ciphertext)
-    print (private_key)
     W = private_key[0]
     Q = private_key[1]
     R = private_key[2]
@@ -139,145 +149,38 @@ def decrypt_mhkc(ciphertext, private_key):
     binary_info_list = []
     decrypted = ""
 
-    #print(ciphertext)
     for C_value in ciphertext:
         ASCII_index = 0
         binary_information = []
+        #calculate C'
         C_p = ((C_value * S) % Q)
         binary_string = ""
-        #print ("C_Value ", C_Value)
-        print ("C_Prime ", C_p)
-        print ("Beginning BINARY_INFO", binary_information)
-
-        #print ("S ", S)
-        #print ("Q ", Q)
-        #C_decipher.append(C_p)
+        #iterating backwards through W so that can find largest W value which is in the subset
         for i in range (len(W)-1, -1, -1):
-            print ("W[i] is ", W[i], i)
+            #if W[i] is part of the subset, append 1
             if C_p >= W[i]:
-                binary_information.insert(0,1)
+                #binary_information.insert(0,1)
                 C_p = C_p - W[i]
-                #print ("W[i] is " , W[i])
-                #print ("CP IS UPDATED ",  C_p)
                 binary_string = "1" + binary_string
-
-                #print ("W[i] is " , W[i])
+            #if W[i] is not part of the subset, append 0.
             elif C_p > 0:
-                #print ("CPRIME IS ", C_p)
-                #print (" I IS ", i)
-                binary_information.insert(0,0)
+                #binary_information.insert(0,0)
                 binary_string = "0" + binary_string
-            print ("BINARY_INFO", binary_information)
-        #binary_info_list.append(binary_string)
-        print(binary_string)
-        print(binary_information)
-        print(int(binary_string, 2))
+            print(binary_string)
+        #convert binary_string value into integer
         ASCII_index = int(binary_string, 2)
-        #print (ASCII_index)
+        print(chr(ASCII_index))
+        #add corresponding ASCII character to decrypted string
         decrypted = decrypted + chr(ASCII_index)
-        #ASCII_index = 0
+    return(decrypted)
 
-        #binary_info_list.append(binary_information)
-        #print("THIS IS INT", int(binary_string, 2))
-        print(decrypted)
-
-    #print (binary_info_list)
-
-'''
-    W = private_key[0]
-    Q = private_key[1]
-    R = private_key[2]
-    S = calc_S(R,Q)
-    C_decipher = []
-    binary_info_list = []
-
-    print(ciphertext)
-    for C_value in ciphertext:
-        binary_information = []
-        binary_string = ""
-        C_p = ((C_value * S) % Q)
-        print(C_value)
-
-        print ("C_Prime ", C_p)
-        print ("S ", S)
-        print ("Q ", Q)
-        #C_decipher.append(C_p)
-
-        for i in range (len(W)-1, -1, -1):
-            print ("CP IS ", C_p)
-            if C_p >= W[i]:
-                binary_information.insert(0,1)
-                C_p = C_p - W[i]
-                #Eprint ("BINARY_INFO", binary_information)
-            else:
-                binary_information.insert(0,0)
-            print ("BINARY_INFO", binary_information)
-        binary_info_list.append(binary_information)
-
-'''
-'''
-        for i in range (len(W)-1, -1, -1):
-            print ("CP ", C_p)
-            if C_p >= W[i]:
-                binary_information.insert(0,1)
-                binary_string = "1" + binary_string
-                C_p = C_p - W[i]
-                #print ("BINARY_INFO", binary_information)
-            elif C_p > 0:
-                binary_information.insert(0,0)
-                binary_string = "0" + binary_string
-            print ("BINARY_INFO", binary_information)
-            #print ("BINARY_STRING", binary_string)
-        print("THIS IS INT", int(binary_string, 2))
-        binary_info_list.append(binary_information)
-'''
-    #print (binary_info_list)
-
-'''
-    W = private_key[0]
-    Q = private_key[1]
-    R = private_key[2]
-    S = calc_S(R,Q)
-    C_decipher = []
-    binary_info_list = []
-
-    print(ciphertext)
-    for C_value in ciphertext:
-        binary_information = []
-        C_p = ((C_value * S) % Q)
-        print(C_value)
-        binary_string = ""
-
-        print ("C_Prime ", C_p)
-        print ("S ", S)
-        print ("Q ", Q)
-        #C_decipher.append(C_p)
-
-        for i in range (len(W)-1, -1, -1):
-            print ("WI IS ", W[i])
-            if C_p >= W[i]:
-                binary_string = "1" + binary_string
-                binary_information.insert(0,1)
-                C_p = C_p - W[i]
-                #Eprint ("BINARY_INFO", binary_information)
-            else:
-                binary_information.insert(0,0)
-                binary_string = "0" + binary_string
-        print ("BINARY_INFO", binary_string)
-        print ("BINARY LIST ", binary_information)
-        binary_info_list.append(binary_information)
-
-    print (binary_info_list)
-
-'''
-
+#method which tests encrypt/decrypt functions for Ceasar and Vignere
 def test(csv_file, function_call):
     output = ""
     with open(csv_file, 'r') as f:
         for line in f:
             line = line.strip()
             split = line.split(',')
-            #print(split)
 
             if function_call == "encrypt_caesar":
                 output = encrypt_caesar(split[0], int(split[1]))
@@ -291,74 +194,28 @@ def test(csv_file, function_call):
             if function_call == "decrypt_vigenere":
                 output = decrypt_vigenere(split[2], split[1])
 
-                print (split[0] == output)
-
-            #if output != split[2]:
-                #print("ERROR: ", output, split[0])
-
-def test_MHKC(csv_file):
-    output = ""
-    with open(csv_file, 'r') as f:
-        for line in f:
-            line = line.strip()
-            split = line.split(',')
-            print(split)
-            output = encrypt_mhkc(split[2], split[1])
-            print(output)
-            print(split[3])
-
-
-'''
- print(split[2],",", split[1], ",", split[0])
-
-    if function_call == "decrypt_caesar":
-        with open(csv_file, 'r') as f:
-            for line in f:
-                line = line.strip()
-                split = line.split(',')
-                print(split)
-                output = decrypt_caesar(split[0], int(split[1]))
-                print (output)
-                if output != split[2]:
-                    print("ERROR: ", output, split[0])
-
-    if function_call == "encrypt_vigenere":
-
-
-'''
+            print (split[0] == output)
 
 def main():
-    #print(encrypt_caesar("Z", 3))
+    #print(encrypt_caesar("", 3))
     #test("encrypt_vigenere.csv", "decrypt_vigenere")
-    #print(decrypt_caesar("DWBB", 2))
-    #print(encrypt_vigenere("HELLOMYNAMEISMANAT", "KAUR"))
+    #print(decrypt_caesar("", 2))
+    #print(encrypt_vigenere("MANAT", "KAUR"))
     #print(decrypt_vigenere("REFCYMSEKMYZCMUEKT", "KAUR"))
     #private_key = generate_private_key()
     #public_key = create_public_key(private_key)
     #private_key = generate_private_key()
     #public_key = create_public_key(private_key)
-    #print(encrypt_mhkc("FOREACHEPSILONGREATERTHANDELTA", public_key))
+    #encrypted = (encrypt_mhkc("Manat", public_key))
+    #print(encrypted)
+    #print("DECRYPTED IS " + decrypt_mhkc(encrypted, private_key))
     #decrypt_mhkc([1129], ((2,7,11,21,42,89,180,354), 881, 588))
     #decrypt_mhkc([2442, 7212, 1936, 5216, 4596, 6402, 206, 5216, 130, 6516, 4786, 826, 7212, 2632, 7022, 1936, 5216, 4596, 750, 5216, 1936, 750, 206, 4596, 2632, 636, 5216, 826, 750, 4596], ((5, 8, 18, 57, 95, 310, 903, 2290, 5423), 9341, 2))
     #print(encrypt_mhkc("MICHAELTHIBODEAUX",(18, 36, 60, 153, 411, 693, 2535, 3957)))
     #test_MHKC("MHKC_tests.csv")
-    decrypt_mhkc([2442, 7212, 1936, 5216, 4596, 6402, 206, 5216, 130, 6516, 4786, 826, 7212, 2632, 7022, 1936, 5216, 4596, 750, 5216, 1936, 750, 206, 4596, 2632, 636, 5216, 826, 750, 4596], ((5, 8, 18, 57, 95, 310, 903, 2290), 9341, 2))
-
-
+    #decrypt_mhkc([2442, 7212, 1936, 5216, 4596, 6402, 206, 5216, 130, 6516, 4786, 826, 7212, 2632, 7022, 1936, 5216, 4596, 750, 5216, 1936, 750, 206, 4596, 2632, 636, 5216, 826, 750, 4596], ((5, 8, 18, 57, 95, 310, 903, 2290), 9341, 2))
 
     # Testing code here
 
 if __name__ == "__main__":
     main()
-
-'''
-Even if you know how to encrypt something, you don't know how to decrypt it.
-Using public key decryption system. Everyone participates in this system.
-1/2 of key is private, 1/2 key is public.
-People will use your public key to send you an encrypted message, and you will
-use your private key to unlock it
-
-If number at the end of the list is less than target, you know everything to the left
-of that number must be included in the subset.
-
-'''
